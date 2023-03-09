9f42516766e70212b97fd3b95ec1d6a34dc44cb0
Lars Serra <serralars@gmail.com>
2023-03-09 17:52:24 +0800

    404

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,13 +1,20 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Image, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const PageNotFound = () => {
   return (
     <main>
-      <h1> 404 Page Not Found !</h1>
-        <Link to='/'>Return to main</Link>
+      <Row className="text-center m-5 p-5" >
+      <Col>
+        <Image src="" alr="logo 404 bg" />
+        <h1> 404 Page Not Found !</h1>
+        <p>We ca't seem to find the page you're looking for</p>
+        <Link to="/">Return to main</Link>
+        </Col>
+      </Row>
     </main>
-  )
-}
+  );
+};
 
-export default PageNotFound
+export default PageNotFound;
